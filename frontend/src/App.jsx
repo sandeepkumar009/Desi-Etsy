32c908f693503110004bdb6e111732d7484ed234
Sandeep Kumar <sandeepkumar75255@gmail.com>
2025-07-06 16:05:25 +0530

    chore: initial project structure (MERN stack)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-orange-500 text-white px-4 py-3 text-5xl rounded-2xl font-bold">Desi Etsy</div>
+      </div>
+    </>
+  )
+}
+
+export default App
