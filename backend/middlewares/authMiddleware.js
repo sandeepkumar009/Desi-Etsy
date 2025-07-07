e9313808c1e30d82c785fba3b13bda99f50e5e14
Sandeep Kumar <sandeepkumar75255@gmail.com>
2025-07-07 22:29:45 +0530

    feat: Implement JWT utility and auth middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,47 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = { id: decoded.userId, role: decoded.role };
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+const artisan = (req, res, next) => {
+  if (req.user && req.user.role === 'artisan') {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an artisan');
+  }
+};
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, artisan, admin };
